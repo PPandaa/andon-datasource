053a24fdc3cbcf98997d61ec8e94a5c4b0e29512
Peter Hu <any99147@gmail.com>
2021-07-01 16:30:09 +0800

    1.0.18 => demo

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ IMAGE_NAME="ifps-andon-datasource"
 DOCKER_REPO="iiicondor/$IMAGE_NAME"
 VERSION="1.0.18"
 
-docker build -t $DOCKER_REPO:$VERSION .
-docker push $DOCKER_REPO:$VERSION
-docker tag $DOCKER_REPO:$VERSION $DOCKER_REPO:dev
-docker push $DOCKER_REPO:dev
-MESSAGE="change V2_Panel9 => add eventLatestResult[\"Parameters\"] != nil"
-echo "[`date "+%Y-%m-%d %H:%M:%S"`] $VERSION => dev {$MESSAGE}" >> ImageInfo.txt
+# docker build -t $DOCKER_REPO:$VERSION .
+# docker push $DOCKER_REPO:$VERSION
+# docker tag $DOCKER_REPO:$VERSION $DOCKER_REPO:dev
+# docker push $DOCKER_REPO:dev
+# MESSAGE="change V2_Panel9 => add eventLatestResult[\"Parameters\"] != nil"
+# echo "[`date "+%Y-%m-%d %H:%M:%S"`] $VERSION => dev {$MESSAGE}" >> ImageInfo.txt
 
-# docker pull $DOCKER_REPO:$VERSION
-# docker tag $DOCKER_REPO:$VERSION $DOCKER_REPO:demo
-# docker push $DOCKER_REPO:demo
-# echo "[`date "+%Y-%m-%d %H:%M:%S"`] $VERSION => demo" >> ImageInfo.txt
+docker pull $DOCKER_REPO:$VERSION
+docker tag $DOCKER_REPO:$VERSION $DOCKER_REPO:demo
+docker push $DOCKER_REPO:demo
+echo "[`date "+%Y-%m-%d %H:%M:%S"`] $VERSION => demo" >> ImageInfo.txt
 
 docker rmi -f $(docker images | grep $DOCKER_REPO | awk '{print $3}')
 docker image prune -f
