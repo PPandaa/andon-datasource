9874e1cdfcbe0576eb010036658de798572dc088
yogageek <xiaoyoga82@gmail.com>
2021-07-08 03:27:33 +0800

    modify switching panel workordersid->subworkordersid

--- middleware/func_sfc_switchpanel.go
+++ middleware/func_sfc_switchpanel.go
@@ -11,8 +11,31 @@ import (
 
 //=get moId
 func GetWorkorderIds(w http.ResponseWriter, r *http.Request) {
-	res := getWorkorderIds()
+	res := getSubWorkorderIds()
 	json.NewEncoder(w).Encode(res)
+}
+
+func getSubWorkorderIds() []map[string]interface{} {
+	trigger := func(i interface{}) ([]byte, error) {
+		url := Url + "/grafana/switchingPanel/subworkorders/id"
+		//convert object to json
+		param := req.BodyJSON(&i)
+		//res就是打api成功拿到的response, 如果打失敗則拿到err
+		res, err := DoAPI("GET", url, param)
+		if err != nil {
+			return nil, err
+		}
+		return res, nil
+	}
+	res, _ := trigger(nil)
+
+	var grafanaData []map[string]interface{}
+	err := json.Unmarshal(res, &grafanaData)
+	if err != nil {
+		glog.Error(err)
+	}
+
+	return grafanaData
 }
 
 //switchingPanel
